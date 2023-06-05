a645e83782dbfcdaf4079cc6e0a74a405ff3abac
Anderson-Sam <77412131+Anderson-Sam@users.noreply.github.com>
2023-06-05 11:54:41 -0700

    added waste per capita

--- js/waste.js
+++ js/waste.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Data for California per capita GDP from 1995 to 2018
   const years = ["1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"];
-  const gdpData = [28759, 30113, 32972, 34786, 37073, 39925, 39901, 40676, 42490, 44644, 47409, 50310, 52383, 53127, 51139, 52357, 53744, 55651, 57987, 60443, 63480, 65513, 69215, 73280	];
+  const wasteData = [0.819264, 0.816117, 0.863644, 0.890916, 0.867828, 0.881186, 0.899346, 0.902766, 0.983344, 0.984560, 1.030070, 1.011261, 0.951962, 0.855896, 0.746973, 0.751967, 0.773099, 0.701883, 0.712172, 0.714546, 0.788888, 0.857989, 0.907319, 0.923701];
 
   // Get the canvas element
-  const canvas = document.getElementById("chart1");
+  const canvas = document.getElementById("chart2");
 
   // Create the chart
   const ctx = canvas.getContext("2d");
@@ -13,10 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
     data: {
       labels: years,
       datasets: [{
-        label: "California Per Capita GDP From 1995 to 2018",
-        data: gdpData,
+        label: "California Per Capita Waste Disposed 1995 to 2018",
+        data: wasteData,
         fill: false,
-        borderColor: "rgb(75, 192, 192)",
+        borderColor: "rgb(75, 75, 192)",
         tension: 0.1
       }]
     },
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
           display: true,
           title: {
             display: true,
-            text: "Per Capita GDP (USD)"
+            text: "Tonns of Waste Disposed"
           }
         }
       }
